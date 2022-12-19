7ccd8bb004ba1855ab01bbec26a9908a35ff8e96
moge <imaamy01@gmail.com>
2022-12-19 19:31:28 +0300

    fixed deprecation

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log("Db Conected")
 }).catch((err) => console.log(err))
