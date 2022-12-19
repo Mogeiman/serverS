99643b757b6d9c379738a55f0261c876493673eb
moge <imaamy01@gmail.com>
2022-12-19 17:52:29 +0300

    loop error fixed

--- server.js
+++ server.js
@@ -1,49 +1,23 @@
-require('dotenv').config('./.env')
-require('./config/db');
 const express = require('express')
 const app = express()
 const cors = require('cors');
 const router = express.Router()
 const cookieParser = require('cookie-parser')
-const session = require('express-session')
 const bodyParser = require('body-parser')
 const path = require('path')
 
-
+require('dotenv').config('./.env')
+require('./config/db');
 //Have no Idea
 global.__basedir=__dirname
 
 //communication between the front and back end
-app.use(cors({
-    origin: "http://localhost:3000",
-    methods: ["GET","POST"],
-    credentials: true
-}))
+app.use(cors())
 
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.use(session({
-    key: "userId",
-    secret: process.env.TOKEN_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie:{
-        expires: 60*60*24,
-    }
 
-}))
-
-app.use(session({
-    key: "userId",
-    secret: process.env.TOKEN_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie:{
-        expires: 60*60*24,
-    }
-
-}))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '/client/build/')))
 require('./routes/route')(app,router)
